067eceada96dcb3ed62b5db64c75cb8c6cd13998
Robert Fuentes <robert.f@ideahubsolutionsinc.com>
2026-02-25 21:02:26 +0800

    Fix: Add missing path import

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { google } = require('googleapis');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 
 app.use(express.json());
